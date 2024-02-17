e948fbb791ceebc058ebd0df0885cee342d93c27
DX-Sumon <dxsumon14567@gmail.com>
2024-02-18 01:20:08 +0600

    score update and life score update

--- JS file/play-ground.js
+++ JS file/play-ground.js
@@ -1,13 +1,3 @@
-function continueGame(){
-    const alphabet = gameLoop();
-    // console.log(alphabet);
-
-    const currentAlphabetElement = document.getElementById('current-alphabet');
-    currentAlphabetElement.innerText = alphabet;
-
-    // Set the keyboard background color
-    setBackgroundColorById(alphabet);
-}
 
 function playNow(){
     hiddenElementById('play-section');
--- JS file/utility.js
+++ JS file/utility.js
@@ -24,4 +24,58 @@ function setBackgroundColorById(elementId){
     element.classList.add('bg-[#f87171]')
     element.classList.add('poppins-medium');
     element.classList.add('text-white');
+ }
+function removeBackgroundColor(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-[#f87171]');
+    element.classList.remove('text-white');
+    element.classList.remove('poppins-medium');
+}
+
+function playerKeyPress(event){
+    const userKeyPressAlphabet = event.key;
+    const displayAlphabets = document.getElementById('current-alphabet');
+    const displayAlphabet = displayAlphabets.innerText;
+    const screenAlphabet = displayAlphabet.toLocaleLowerCase();
+    console.log(`user keypress ${userKeyPressAlphabet} But you need press ${screenAlphabet}`);
+    let score = 0;
+    if(userKeyPressAlphabet === screenAlphabet){
+        console.log('You get a score');
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreNumber = currentScoreElement.innerText;
+        const score = parseInt(currentScoreNumber);
+        const newScore = score + 1;
+        currentScoreElement.innerText = newScore;
+        removeBackgroundColor(screenAlphabet);
+        continueGame();
+    }
+    else{
+        hiddenElementById('play-ground');
+        addElementById('score-section');
+        const lifeScoreElement = document.getElementById('life-score');
+        const lifeScoreText = lifeScoreElement.innerText;
+        const lifeScore = parseInt(lifeScoreText);
+        const newLifeScore = lifeScore - 1;
+        lifeScoreElement.innerText= newLifeScore;
+    }
+}
+
+document.addEventListener('keyup', playerKeyPress);
+
+function continueGame(){
+    const alphabet = gameLoop();
+    // console.log(alphabet);
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // Set the keyboard background color
+    setBackgroundColorById(alphabet);
+}
+
+function playAgain(){
+    hiddenElementById('score-section');
+    addElementById('play-ground');
+    const currentScore = document.getElementById('current-score');
+    currentScore.innerText = 0;
 }
